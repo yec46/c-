b9a237a18ec9cd12bc4cc4f640024894cfa72a8b
yec46 <56663011+yec46@users.noreply.github.com>
2022-08-08 00:11:16 +0900

    24_증가연산자, 감소연산자와 대입연산자의 압축

---
+++ 24.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+
+            Console.WriteLine(x += 2);
+            Console.WriteLine(x -= 8);
+            Console.WriteLine(x *= 3);
+            Console.WriteLine(x /= 2);
+            // ++x는 x를 먼저 증가시키고 사용
+            Console.WriteLine(x ++);
+            // 현재 값을 먼저 사용하고 나서 x 값 증가
+            Console.WriteLine(++ x);
+
+            Console.WriteLine(-- x);
+        }
+    }
+}
