5a35a3831aaa4d51d9a3816d1e195cf57aad382b
yec46 <56663011+yec46@users.noreply.github.com>
2022-10-13 17:18:46 +0900

    값 형식과 참조형식, ref 키워드

---
+++ A034.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "before passing";
+            Console.WriteLine(s);
+
+            Test(s);
+            Console.WriteLine(s);
+            Test(ref s);
+            Console.WriteLine(s);
+        }
+
+        public static void Test(string s)
+        {
+            s = "after passing";
+        }
+
+        public static void Test(ref string s)
+        {
+            s = "after passing";
+        }
+
+        
+    }
+}
